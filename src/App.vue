de6726067f6f18404190ec24c672ac4805429b0b
gaozhan <1312591700@qq.com>
2021-01-21 14:36:09 +0800

    create demo

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+
+    {{$store.state.name}}
+    {{$store.state.a.name}}
+    {{$store.state.b.name}}
+    <button @click="$store.commit('changeName')">点击</button>
+    <button @click="$store.commit('a/changeName')">changeA</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "App",
+  
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/store/b.js
@@ -0,0 +1,11 @@
+export default{
+    namespaced:true,
+    state:{name:"小B"},
+    mutations: {
+        changeName(state){
+            state.name ="小小B";
+        }
+    },
+    actions: {},
+    getteers:{}
+};
